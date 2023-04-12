23642b2bc27b610d64c520fbcc87f804842df0e1
Paul Thomas <paulthomas2107@gmail.com>
2023-04-12 13:11:28 +0100

    More

---
+++ hellothere.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+FIRST_NAME=Paul
+LAST_NAME=Thomas
+
+echo Hi, $FIRST_NAME $LAST_NAME
---
+++ interactiveshell.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+echo What is your first name
+read FIRST_NAME
+echo What is your surname
+read SURNAME
+
+echo Hello $FIRST_NAME $SURNAME
